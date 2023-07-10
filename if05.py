15a0a51bed2465bae7619850d5f42253e2a144ad
ozodbektoshpulatov <toshpulatovozodbek96@gmail.com>
2023-07-10 08:43:21 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//1000
+    b=n//1000%10
+    c=n//100%10
+    d=n//10%10
+    e=n%10
+    if a>=b and a>=c and a>=d and a>=e:
+        return a
+    elif b>=a and b>=c and b>=d and b>=e:
+        return b
+    elif c>=a and c>=b and c>=d and c>=e:
+        return c
+    elif d>=a and d>=b and d>=c and d>=e:
+        return d
+    elif e>=a and e>=b and e>=d and e>=c:
+        return e
+print(main(67899))
